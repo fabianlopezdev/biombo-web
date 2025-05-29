8b44a50c62b41e5b4fd3b94a36f1a0b297e45fe1
fabianlopezdev <fabianlopez@pm.me>
2025-05-29 17:25:39 -0400

    feat: add i18n constants and singleton creation script

---
+++ sanity-studio/constants/i18n.ts
@@ -0,0 +1,13 @@
+export const LANGUAGES = [
+  {id: 'ca', title: 'Català'}, // default language
+  {id: 'es', title: 'Español'},
+  {id: 'en', title: 'English'},
+]
+
+export const SINGLETONS = [
+  {id: 'homePage', _type: 'homePage', title: 'Home Page'},
+  {id: 'header', _type: 'header', title: 'Header'},
+  {id: 'projectsPage', _type: 'projectsPage', title: 'Projects Page'},
+  {id: 'aboutUsPage', _type: 'aboutUsPage', title: 'About Us Page'},
+  {id: 'contactPage', _type: 'contactPage', title: 'Contact Page'},
+]
