f5f82f0f82a97d64895baffdca8af6f2ed399b8a
fabianlopezdev <fabianlopez@pm.me>
2025-05-31 07:38:03 -0400

    feat: add dynamic content sections to project schema

--- sanity-studio/schemaTypes/index.ts
+++ sanity-studio/schemaTypes/index.ts
@@ -4,17 +4,21 @@ import { localePortableText } from './localePortableText'
 import { localeSlug } from './localeSlug'
 import { header, navigationItemType } from './header'
 import { homePage, heroSection, projectsSection, aboutSection, servicesSection } from './homePage'
-import { projects } from './projects'
+import { projects, imageSection, textBlock, smallTextBlock } from './projects'
 import { siteSettings } from './siteSettings'
 import { projectsPage } from './projectsPage'
 import { aboutUsPage } from './aboutUsPage'
 import { contactPage } from './contactPage'
+import { serviceCategory } from './serviceCategory'
+import { client } from './client'
 
 export const schemaTypes = [
   // Documents
   header,
   homePage,
   projects,
+  serviceCategory,
+  client,
   siteSettings,
   projectsPage,
   aboutUsPage,
@@ -29,4 +33,7 @@ export const schemaTypes = [
   projectsSection,
   aboutSection,
   servicesSection,
+  imageSection,
+  textBlock,
+  smallTextBlock,
 ]
